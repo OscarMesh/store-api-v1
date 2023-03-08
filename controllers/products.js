f1fdd49f676d6c85a095ddc487dfed87b2a581cb
osacrMesh <okerekeifeanyi65@gmail.com>
2023-03-08 15:57:16 +0100

    first commit

---
+++ controllers/products.js
@@ -0,0 +1,27 @@
+const Product = require("../models/Product");
+
+const getAllProductsStatic = async (req, res) => {
+  const products = await Product.find({
+    name: "vase table ",
+  });
+  res.status(200).json({ products });
+};
+
+const getAllProducts = async (req, res) => {
+  const { featured, company } = req.query;
+  const queryObject = {};
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  }
+    if (company) {
+    queryObject.company = company;
+    }
+  console.log(queryObject);
+  const products = await Product.find(queryObject);
+  res.status(200).json({ products, nbHits: products.length });
+};
+
+module.exports = {
+  getAllProducts,
+  getAllProductsStatic,
+};
